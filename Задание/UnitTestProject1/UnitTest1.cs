a6b7e75852b4e8754c1d37ffa52e1a65e40db009
KristinaN98 <43462664+KristinaN98@users.noreply.github.com>
2018-10-23 22:23:23 +0500

    рефакторинг, автотест

---
+++ Задание/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Задание;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string S = "Привет игорь";
+            string expected = "рПвитеи огьр";
+            char[] res = Program.shifr(S);
+            string actiual = Convert.ToString(res);
+            Assert.AreEqual(expected,actiual);
+        }
+    }
+}
--- Задание/Задание/Program.cs
+++ Задание/Задание/Program.cs
@@ -6,33 +6,24 @@ using System.Threading.Tasks;
 
 namespace Задание
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-           
+
             Console.WriteLine("Введите строку: ");//1
-            string  S = "";//2
+            string S = "";//2
             S = Convert.ToString(Console.ReadLine());
-            char[] c = S.ToCharArray(); //3
-            shifr(c);//4
-            string str = "";//5
-            for (int i = 0; i < c.Length; i++)//6
-            {
-                str = str + c[i].ToString();//7
-            }//8
+            char[] c = shifr(S);
+            string str = vivod(c);
             Console.WriteLine("Зашифрованная строка: {0}", str);//9
-            shifr(c);//10
-            string str1 = "";//11
-            for (int i = 0; i < c.Length; i++)//12
-            {
-                str1 = str1 + c[i].ToString();//13
-            }//14
-            Console.WriteLine("Разшифрованная строка: {0}", str1);//15
+            rashfr(c);
+            string str1 = vivod(c);
+            Console.WriteLine("Расшифрованная строка: {0}", str1);//15
             Console.ReadLine();//16
         }
 
-        private static void shifr(char[] c)
+        private static void rashfr(char[] c)
         {
             for (int i = 0; i < c.Length; i = i + 2) //1
             {
@@ -48,5 +39,38 @@ namespace Задание
                 }//9
             }//10
         }
+
+        public static char[] shifr(string S)
+        {
+            char[] c = S.ToCharArray(); //3
+            for (int i = 0; i < c.Length; i = i + 2) //1
+            {
+                try //2
+                {
+                    char a1 = c[i];//3
+                    c[i] = c[i + 1];//4
+                    c[i + 1] = a1;//5
+                }//6
+                catch (Exception)//7
+                {
+                    break;//8
+                }//9
+            }//10
+
+            return c;
+        }
+
+        private static string vivod(char[] c)
+        {
+            string str = "";//5
+            for (int i = 0; i < c.Length; i++)//6
+            {
+                str = str + c[i].ToString();//7
+            }//8
+
+            return str;
+        }
+
+       
+        }
     }
-}
